debb302ed4e6d8a2f3bfc64d936ef8be116b60cb
Your Name <your_email@example.com>
2024-08-15 22:00:38 +0900

    GNB

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LocationModal from "./Location";
 
 export default function Navbar() {
     return (
-        <nav className="w-full w-[1200px] flex justify-between items-center py-4">
+        <nav className=" w-[1200px] flex justify-between items-center py-4">
             <div className="text-gray-scale-1 text-xl font-bold">
             <Link to="/" className="text-gray-scale-1 text-lg">Logo</Link>
             </div>
