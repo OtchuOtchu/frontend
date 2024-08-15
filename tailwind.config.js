87879ef6b9e0e97e0dd6c75f1475edb5eeb6dec0
Your Name <your_email@example.com>
2024-08-15 14:33:53 +0900

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'black-1': '#FF5500',
+      'gray-scale-1': '#1E1E1E',
+      'gray-scale-2': '#414244',
+      'gray-scale-3': '#50545E',
+    },
+    borderRadius: {
+      DEFAULT: '14px',
+    },
   },
   plugins: [],
 }
