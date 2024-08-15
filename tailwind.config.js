f806196ce47f29017dd2d9017fe866d46c51b892
Your Name <your_email@example.com>
2024-08-15 14:39:56 +0900

    Revert "tailwind"
    This reverts commit 87879ef6b9e0e97e0dd6c75f1475edb5eeb6dec0.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      'black-1': '#FF5500',
-      'gray-scale-1': '#1E1E1E',
-      'gray-scale-2': '#414244',
-      'gray-scale-3': '#50545E',
-    },
-    borderRadius: {
-      DEFAULT: '14px',
-    },
+    extend: {},
   },
   plugins: [],
 }
