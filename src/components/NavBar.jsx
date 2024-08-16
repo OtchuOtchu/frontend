e4e9aac21a82b6ec400eaa8d4403ba9ea91f5579
Your Name <your_email@example.com>
2024-08-16 09:56:59 +0900

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import LocationModal from "./Location"; // LocationModal 컴포넌트를 import 합니다
 
 export default function Navbar() {
-    return (
-        <nav className=" w-[1200px] flex justify-between items-center py-4">
-            <div className="text-gray-scale-1 text-xl font-bold">
-            <Link to="/" className="text-gray-scale-1 text-lg">Logo</Link>
-            </div>
-        <div className="flex gap-10">
-        <div className="text-gray-scale-1 text-lg cursor-pointer">Location</div>
-            <Link to="/mypage" className="text-gray-scale-1 text-lg">My Page</Link>
-        </div>
-      </nav>
+    const [isLocationModalOpen, setIsLocationModalOpen] = useState(false);
 
+    const openLocationModal = () => {
+        setIsLocationModalOpen(true);
+    };
+
+    const closeLocationModal = () => {
+        setIsLocationModalOpen(false);
+    };
+
+    return (
+        <nav className="w-full max-w-[1200px] min-w-[340px] mx-auto flex justify-between items-center px-8 py-4">
+            <div className="text-gray-scale-1 text-xl font-bold">
+                <Link to="/" className="text-gray-scale-1 text-lg">Logo</Link>
+            </div>
+            <div className="flex gap-10">
+                <div 
+                    className="text-gray-scale-1 text-lg cursor-pointer"
+                    onClick={openLocationModal}
+                >
+                    Location
+                </div>
+                <Link to="/mypage" className="text-gray-scale-1 text-lg">My Page</Link>
+            </div>
+            {isLocationModalOpen && (
+                <LocationModal onClose={closeLocationModal} />
+            )}
+        </nav>
     )
 }
