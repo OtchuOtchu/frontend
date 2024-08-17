940b9437791d892719be44b8ab8920f2fceaa2f4
Your Name <your_email@example.com>
2024-08-17 11:09:57 +0900

    solve whitebutton

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,21 +1,6 @@
 const WhiteButton = ({ label, onClick }) =>{
     return (
-        
-<<<<<<< Updated upstream
-            <div 
-            className="w-[441px] h-[72px] px-[100px] 
-            bg-white text-black py-5 
-            border border-neutral-300 justify-center items-center 
-            inline-flex
-            hover:bg-gray-100">
-
-                     <div 
-                     onClick={onClick} 
-                     className="Google text-black text-xl font-semibold font-['Inter'] leading-normal">
-                        {label}
-                    </div>
-            </div>
-=======
+     
             <button
             className="w-[441px] h-[72px] px-[100px] py-5 
             bg-white text-black 
@@ -30,7 +15,7 @@ const WhiteButton = ({ label, onClick }) =>{
                         {label}
                     </div>
             </button>
->>>>>>> Stashed changes
+
         
     )
 }
