19986b4774dd4940819d7be64f678557dcab3fd7
Your Name <your_email@example.com>
2024-08-17 10:51:38 +0900

    clean black button

--- src/components/BlackButton.jsx
+++ src/components/BlackButton.jsx
@@ -1,29 +1,13 @@
-<<<<<<< Updated upstream
 
 const BlackButton = ({ label, onClick }) => {
     return (
         <button
             onClick={onClick}
-            className="w-[441px] h-[72px] bg-black text-white text-xl font-semibold leading-normal flex justify-center items-center gap-2.5 px-[100px] py-5"
-=======
-import React from 'react';
-
-export default function BlackButton({ label, onClick, className }) {
-    return (
-        <button
-            onClick={onClick}
-            className={`w-[441px] h-[72px] bg-black 
-                text-white text-xl font-semibold leading-normal flex justify-center items-center 
-                gap-2.5 px-[100px] py-5 rounded-sm ${className}`}
->>>>>>> Stashed changes
+            className="w-[441px] h-[72px] bg-black text-white text-xl font-semibold leading-normal flex justify-center items-center gap-2.5 px-[100px] py-5 rounded-lg"
         >
             {label}
         </button>
     );
-<<<<<<< Updated upstream
 };
 
 export default BlackButton;
-=======
-};
->>>>>>> Stashed changes
