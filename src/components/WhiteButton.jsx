d69eb95fcfa8ab1a15620c2c0a2c03aa07591bca
Your Name <your_email@example.com>
2024-08-17 09:32:14 +0900

    whitebutton

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,6 +1,7 @@
 const WhiteButton = ({ label, onClick }) =>{
     return (
         
+<<<<<<< Updated upstream
             <div 
             className="w-[441px] h-[72px] px-[100px] 
             bg-white text-black py-5 
@@ -14,6 +15,22 @@ const WhiteButton = ({ label, onClick }) =>{
                         {label}
                     </div>
             </div>
+=======
+            <button
+            className="w-[441px] h-[72px] px-[100px] py-5 
+            bg-white text-black 
+            border border-neutral-300 justify-center items-center 
+            inline-flex
+            hover:bg-gray-100 cursor-pointer">
+
+                     <div 
+                     onClick={onClick} 
+                     className="Google text-black text-xl font-semibold 
+                     leading-normal">
+                        {label}
+                    </div>
+            </button>
+>>>>>>> Stashed changes
         
     )
 }
