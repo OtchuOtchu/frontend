7e36841765aeecf5c9ecb6daa67e571dcf5aa9b6
Your Name <your_email@example.com>
2024-08-17 10:53:07 +0900

    black button last

--- src/components/BlackButton.jsx
+++ src/components/BlackButton.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 
-const BlackButton = ({ label, onClick }) => {
+export default function BlackButton({ label, onClick, className }) {
     return (
         <button
             onClick={onClick}
-            className="w-[441px] h-[72px] bg-black text-white text-xl font-semibold leading-normal flex justify-center items-center gap-2.5 px-[100px] py-5 rounded-lg"
+            className={`w-[441px] h-[72px] bg-black 
+                text-white text-xl font-semibold leading-normal flex justify-center items-center 
+                gap-2.5 px-[100px] py-5 rounded-sm ${className}`}
         >
             {label}
         </button>
     );
 };
 
-export default BlackButton;
