b2149bbb8ed672dbe65db23b6d2f205a033ab8a3
Your Name <your_email@example.com>
2024-08-17 10:59:34 +0900

    white

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,19 +1,21 @@
 const WhiteButton = ({ label, onClick }) =>{
     return (
-        
-            <div 
-            className="w-[441px] h-[72px] px-[100px] 
-            bg-white text-black py-5 
+    
+            <button
+            className="w-[441px] h-[72px] px-[100px] py-5 
+            bg-white text-black 
             border border-neutral-300 justify-center items-center 
             inline-flex
-            hover:bg-gray-100">
+            hover:bg-gray-100 cursor-pointer">
 
                      <div 
                      onClick={onClick} 
-                     className="Google text-black text-xl font-semibold font-['Inter'] leading-normal">
+                     className="Google text-black text-xl font-semibold 
+                     leading-normal">
                         {label}
                     </div>
-            </div>
+            </button>
+
         
     )
 }
