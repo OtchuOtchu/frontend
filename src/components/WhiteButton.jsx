87c6564a5b9250963c5d56644acf6a5873d5684b
Your Name <your_email@example.com>
2024-08-17 11:02:38 +0900

    CSS unification

--- src/components/WhiteButton.jsx
+++ src/components/WhiteButton.jsx
@@ -1,6 +1,33 @@
 const WhiteButton = ({ label, onClick }) =>{
     return (
+<<<<<<< HEAD
     
+            <button
+            className="w-[441px] h-[72px] px-[100px] py-5 
+            bg-white text-black 
+=======
+        
+<<<<<<< Updated upstream
+            <div 
+            className="w-[441px] h-[72px] px-[100px] 
+            bg-white text-black py-5 
+>>>>>>> Location
+            border border-neutral-300 justify-center items-center 
+            inline-flex
+            hover:bg-gray-100 cursor-pointer">
+
+                     <div 
+                     onClick={onClick} 
+                     className="Google text-black text-xl font-semibold 
+                     leading-normal">
+                        {label}
+                    </div>
+<<<<<<< HEAD
+            </button>
+
+=======
+            </div>
+=======
             <button
             className="w-[441px] h-[72px] px-[100px] py-5 
             bg-white text-black 
@@ -15,7 +42,8 @@ const WhiteButton = ({ label, onClick }) =>{
                         {label}
                     </div>
             </button>
-
+>>>>>>> Stashed changes
+>>>>>>> Location
         
     )
 }
