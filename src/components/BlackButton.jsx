452c97e26fd0445e90a2d9c6c4ab07a84cc489f1
Your Name <your_email@example.com>
2024-08-17 11:03:35 +0900

    solve black button

--- src/components/BlackButton.jsx
+++ src/components/BlackButton.jsx
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-
-const BlackButton = ({ label, onClick }) => {
-    return (
-        <button
-            onClick={onClick}
-            className="w-[441px] h-[72px] bg-black text-white text-xl font-semibold leading-normal flex justify-center items-center gap-2.5 px-[100px] py-5"
-=======
 import React from 'react';
 
 export default function BlackButton({ label, onClick, className }) {
@@ -15,15 +7,9 @@ export default function BlackButton({ label, onClick, className }) {
             className={`w-[441px] h-[72px] bg-black 
                 text-white text-xl font-semibold leading-normal flex justify-center items-center 
                 gap-2.5 px-[100px] py-5 rounded-sm ${className}`}
->>>>>>> Stashed changes
         >
             {label}
         </button>
     );
-<<<<<<< Updated upstream
 };
 
-export default BlackButton;
-=======
-};
->>>>>>> Stashed changes
