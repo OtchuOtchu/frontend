a8c8dbdd669addc97fb0aaad3ca686053b26fdc3
pop <popramel25@gmail.com>
2024-08-20 10:15:03 +0900

    정호 더미데이터 계정 추가

--- src/store/PeopleStore.jsx
+++ src/store/PeopleStore.jsx
@@ -2,14 +2,25 @@ import create from 'zustand';
 
 // 사람 더미 데이터
 const initialPeople = [
-  { nickname: "파송탁", gender: "Female", email: 'sh1220@hanyang.ac.kr',
-     style: "Casual", height: 165, weight: 55 },
+  {
+    nickname: "파송탁", gender: "Female", email: 'sh1220@hanyang.ac.kr',
+    style: "Casual", height: 165, weight: 55
+  },
 
-  { nickname: "Sunny", gender: "Male", email: 'seond0112@naver.com',
-     style: "Sporty", height: 180, weight: 75 },
+  {
+    nickname: "Sunny", gender: "Male", email: 'seond0112@naver.com',
+    style: "Sporty", height: 180, weight: 75
+  },
 
-  { nickname: "Blossom", gender: "Female", email: 'sh1220@hanyang.ac.kr',
-     style: "Elegant", height: 170, weight: 60 }
+  {
+    nickname: "Blossom", gender: "Female", email: 'sh1220@hanyang.ac.kr',
+    style: "Elegant", height: 170, weight: 60
+  },
+
+  {
+    nickname: "pop", gender: "Female", email: 'popramel@hanyang.ac.kr',
+    style: "Elegant", height: 170, weight: 60
+  }
 ];
 
 // Zustand로 스토어 생성
@@ -18,17 +29,17 @@ const usePeopleStore = create((set) => ({
   loggedInUser: null,
 
   setPeople: (newPeople) => set({ people: newPeople }),
-   
+
   // 새로운 사용자 정보를 저장하는 함수
   setLoggedInUser: (user) => set({ loggedInUser: user }),
-    // 사용자 정보를 업데이트하는 함수
-    updateUser: (updatedUser) =>
-      set((state) => ({
-        people: state.people.map((person) =>
-          person.email === updatedUser.email ? updatedUser : person
-        ),
-        loggedInUser: updatedUser, // 업데이트된 정보를 현재 로그인된 사용자로 설정
-      })),
+  // 사용자 정보를 업데이트하는 함수
+  updateUser: (updatedUser) =>
+    set((state) => ({
+      people: state.people.map((person) =>
+        person.email === updatedUser.email ? updatedUser : person
+      ),
+      loggedInUser: updatedUser, // 업데이트된 정보를 현재 로그인된 사용자로 설정
+    })),
 }));
 
 export default usePeopleStore;
