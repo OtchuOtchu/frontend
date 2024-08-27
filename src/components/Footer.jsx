1b53406c92215f7067739a10004519d7a6bfcc73
Your Name <your_email@example.com>
2024-08-28 01:53:19 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <div className="max-w-full h-[100px] bg-gray-300 text-white px-4">
+        <div className="max-w-full mt-8 h-[100px] bg-gray-300 text-white px-4">
             
             © 2024 otchuotchu. All rights reserved.
             
